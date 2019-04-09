00fb248bdbb67ae2da509cc2978e1a7f0d629b5b
nnecec <nnecec@outlook.com>
2019-04-09 12:43:00 +0800

    chore: add nav isSelected

--- src/pages/shared/Nav.js
+++ src/pages/shared/Nav.js
@@ -3,19 +3,42 @@ import { Pane, Tablist, Tab } from 'evergreen-ui'
 import { connect } from 'react-redux'
 import { push } from 'connected-react-router'
 
-@connect()
+@connect(state => ({
+  router: state.router
+}))
 export default class Nav extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      navList: [{
+        mean: '主页',
+        link: '/'
+      }, {
+        mean: '图片',
+        link: '/imageKiller'
+      }, {
+        mean: '文章',
+        link: '/articleGuard'
+      }]
+    }
+  }
 
   handleLink = (route) => {
     this.props.dispatch(push(route))
   }
   render() {
-    console.log(this.props)
+    const { navList } = this.state
+    const { router } = this.props
+    const pathname = router.location.pathname
+
     return (<Pane elevation={2} padding={8} marginBottom={16}>
       <Tablist flexBasis={240} >
-        <Tab is="a" onClick={() => this.handleLink('/')}>主页</Tab>
-        <Tab is="a" onClick={() => this.handleLink('/imageKiller')}>图片</Tab>
-        <Tab is="a" onClick={() => this.handleLink('/articleGuard')} >文章</Tab>
+        {
+          navList.map(nav => (
+            <Tab onSelect={() => this.handleLink(nav.link)} isSelected={pathname === nav.link}>{nav.mean}</Tab>
+          ))
+        }
       </Tablist>
     </Pane>)
   }
